a115d429603e3c35767ce542e2c444cbaae9ed9f
Carlos <carlosicaraballo1999@gmail.com>
2025-02-03 16:53:21 +0000

    82. Personalizando error 404

--- src/main/java/com/carlos/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/carlos/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
 
@@ -18,7 +19,17 @@ public class HandlerExceptionController {
     error.setError("Error division por cero!");
     error.setMessage(ex.getMessage());
     error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+  }
+
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public ResponseEntity<Error> notFoundEx(NoHandlerFoundException ex) {
+    Error error = new Error();
+    error.setDate(new Date());
+    error.setError("Api rest no encontrada!");
+    error.setMessage(ex.getMessage());
+    error.setStatus(HttpStatus.NOT_FOUND.value());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
   }
 
 }
